712baff48c822ca33630184bb126594713b4912e
Your Name <ariruizmartinez@gmail.com>
2024-10-08 17:39:33 +0200

    commit inicial

---
+++ hackato/hackato/src/test/java/com/ari/hackato/HackatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ari.hackato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
